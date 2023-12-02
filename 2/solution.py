e99670c440ec120abcd6cb00af29ab8d9ba3e441
Alice Lieutier <alice.lieutier@gmail.com>
2023-12-02 15:18:50 +0000

    cleanup and dry

--- 2/solution.py
+++ 2/solution.py
@@ -2,7 +2,6 @@
 import os
 import re
 from collections import namedtuple
-from functools import reduce
 
 TEST_FILE = f'{os.path.dirname(__file__)}/test_input'
 INPUT_FILE = f'{os.path.dirname(__file__)}/input'
@@ -16,7 +15,11 @@ def to_game(line):
   blue = max(int(number) for number in re.findall('(\d+) blue', line))
   return Game(id, red, green, blue)
 
-# playable with only 12 red cubes, 13 green cubes, and 14 blue cubes
+def parse_file(file):
+  with open(file) as input:
+    return [to_game(line.strip()) for line in input.readlines()]
+
+# Is playable with only 12 red cubes, 13 green cubes, and 14 blue cubes
 def is_possible(game):
   return game.red <= 12 and game.green <= 13 and game.blue <= 14
 
@@ -25,15 +28,13 @@ def power(game):
   return game.red * game.green * game.blue
 
 def part_1(file):
-  with open(file) as input:
-    games = (to_game(line.strip()) for line in input.readlines())
-    possible_games = list(filter(lambda game: is_possible(game), games))
-    return sum(game.id for game in possible_games)
+  games = parse_file(file)
+  possible_games = filter(is_possible, games)
+  return sum(game.id for game in possible_games)
   
 def part_2(file):
-  with open(file) as input:
-    games = (to_game(line.strip()) for line in input.readlines())
-    return sum(power(game) for game in games)
+  games = parse_file(file)
+  return sum(power(game) for game in games)
 
 # Solution
 print(part_1(INPUT_FILE)) # 2006
