b612c5c429f0c78549e2ddb8036f8b84fbc2f002
Alice Lieutier <alice.lieutier@gmail.com>
2023-12-04 10:38:18 +0000

    day 4 part 2

--- 4/solution.py
+++ 4/solution.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python3
 import os
 import re
+from functools import reduce
 
 TEST_FILE = f'{os.path.dirname(__file__)}/test_input'
 INPUT_FILE = f'{os.path.dirname(__file__)}/input'
@@ -10,11 +11,11 @@ def to_card(line):
   id, first_half, second_half = match.groups()
   winning = {int(number) for number in re.findall(r'(\d+)', first_half)}
   appeared = {int(number) for number in re.findall(r'(\d+)', second_half)}
-  return id, winning, appeared
+  return int(id), winning, appeared
 
 def parse_file(file):
   with open(file) as input:
-    return (to_card(line.strip()) for line in input.readlines())
+    return [to_card(line.strip()) for line in input.readlines()]
   
 def worth(card):
   _, winning, appeared = card
@@ -23,12 +24,30 @@ def worth(card):
     return 2**(nb_in_common - 1)
   return 0
 
+def count_cards(cards):
+  def aux(counter, card):
+    id,  winning, appeared = card
+    nb_in_common = len(winning & appeared)
+    for i in range(id + 1, id + nb_in_common + 1):
+      counter[i] += counter[id]
+    return counter
+  
+  counter = reduce(aux, cards, {id: 1 for id, _ , _ in cards})
+  return sum(counter.values())
+
 def part_1(file):
   cards = parse_file(file)
   return sum(worth(card) for card in cards)
+
+def part_2(file):
+  cards = parse_file(file)
+  return count_cards(cards)
   
 # Solution
 print(part_1(INPUT_FILE)) # 18619
+print(part_2(INPUT_FILE)) # 8063216
 
 # Tests
 assert part_1(TEST_FILE) == 13
+assert part_2(TEST_FILE) == 30
+
