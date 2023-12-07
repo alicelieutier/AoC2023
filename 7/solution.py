def889db396fe4dd7ea1765effd4acb0bc3df685
Alice Lieutier <alice.lieutier@gmail.com>
2023-12-07 09:35:57 +0000

    day 7 part 2

--- 7/solution.py
+++ 7/solution.py
@@ -14,41 +14,77 @@ def parse_hand(line):
 def parse_file(file):
   with open(file) as input:
     return [parse_hand(line.strip()) for line in input.readlines()]
+  
+def lexico_key(order):
+  def aux(hand):
+    hand, _ = hand
+    return tuple(order.index(letter) for letter in hand)
+  return aux
 
-ORDER = '23456789TJQKA'
-def lexico_key(hand):
-  hand, _ = hand
-  return tuple(ORDER.index(letter) for letter in hand)
-
-# Five of a kind, where all five cards have the same label: AAAAA
-# Four of a kind, where four cards have the same label and one card has a different label: AA8AA
-# Full house, where three cards have the same label, and the remaining two cards share a different label: 23332
-# Three of a kind, where three cards have the same label, and the remaining two cards are each different from any other card in the hand: TTT98
-# Two pair, where two cards share one label, two other cards share a second label, and the remaining card has a third label: 23432
-# One pair, where two cards share one label, and the other three cards have a different label from the pair and each other: A23A4
-# High card, where all cards' labels are distinct: 23456
-def type_key(hand):
-  hand, _ = hand
-  counter = {card: 0 for card in set(hand)}
-  for card in hand: counter[card] += 1
+def hand_value(cards):
+  counter = {card: 0 for card in set(cards)}
+  for card in cards:
+    counter[card] += 1
   match tuple(sorted(counter.values())):
-    case 1,1,1,1,1: return 1
-    case 1,1,1,2: return 2
-    case 1,2,2: return 3
-    case 1,1,3: return 4
-    case 2,3: return 5
-    case 1,4: return 6
-    case (5,): return 7
-    case _: print(f"Help! couldn't score hand: {hand, counter, tuple(sorted(counter.values()))}")
+    case 1,1,1,1,1: return 1 # High card
+    case 1,1,1,2: return 2 # One pair
+    case 1,2,2: return 3 # Two pair
+    case 1,1,3: return 4 # Three of a kind
+    case 2,3: return 5 # Full house
+    case 1,4: return 6 # Four of a kind
+    case (5,): return 7 # Five of a kind
+    case _: print(f"Help! couldn't score hand: {cards, counter, tuple(sorted(counter.values()))}")
+
+def type_key(hand):
+  cards, _ = hand
+  return hand_value(cards)
+
+def type_key_with_joker(hand):
+  cards, _ = hand
+  if 'J' not in cards: return hand_value(cards)
+
+  other_cards = {card for card in cards if card != 'J'}
+  match len(other_cards):
+    case 4: return 2 # One pair
+    case 3: return 4 # Three of a kind
+    case 2: # could result in a full house or a four of a kind
+      if cards.count('J') == 1:
+        return 5 if hand_value(cards) == 3 else 6
+      return 6
+    case _:
+      return 7 # Five of a kind
+
+assert(type_key_with_joker(('4QT44',0))) == 4 # Three of a kind
+assert(type_key_with_joker(('2JQKA',0))) == 2 # One Pair
+assert(type_key_with_joker(('96A89',0))) == 2 # One Pair
+assert(type_key_with_joker(('QQQJA',0))) == 6 # Four of a kind
+assert(type_key_with_joker(('QQJJA',0))) == 6 # Four of a kind
+assert(type_key_with_joker(('235JA',0))) == 2 # One pair
+assert(type_key_with_joker(('23JAA',0))) == 4 # Three of a kind
+assert(type_key_with_joker(('QJ4J8',0))) == 4 # Three of a kind
+assert(type_key_with_joker(('22JAA',0))) == 5 # Full house
+assert(type_key_with_joker(('QQQJJ',0))) == 7 # Five of a kind
+assert(type_key_with_joker(('JJJJJ',0))) == 7 # Five of a kind
+assert(type_key_with_joker(('23JJJ',0))) == 6 # Four of a kind
+assert(type_key_with_joker(('2222J',0))) == 7 # Five of a kind
+assert(type_key_with_joker(('2J22J',0))) == 7 # Five of a kind
 
 def part_1(file):
   hands = parse_file(file)
-  hands.sort(key=lexico_key)
+  hands.sort(key=lexico_key('23456789TJQKA'))
   hands.sort(key=type_key)
+  return sum((index+1) * bid for index, (_, bid) in enumerate(hands))
+
+def part_2(file):
+  hands = parse_file(file)
+  hands.sort(key=lexico_key('J23456789TQKA'))
+  hands.sort(key=type_key_with_joker)
   return sum((index+1) * bid for index, (_, bid) in enumerate(hands))
 
 # Solution
 print(part_1(INPUT_FILE)) # 251058093
+print(part_2(INPUT_FILE)) # 249781879
 
 # Tests
 assert part_1(TEST_FILE) == 6440
+assert part_2(TEST_FILE) == 5905
