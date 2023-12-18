05b5a68c4db15d91775ea27749dca3dea470d1dd
Alice Lieutier <alice.lieutier@gmail.com>
2023-12-18 15:19:22 +0000

    day 17 part 2 slow

--- 17/solution.py
+++ 17/solution.py
@@ -1,9 +1,6 @@
 #! /Library/Frameworks/Python.framework/Versions/3.12/bin/python3
 import os
-# from itertools import count
-# from functools import cache
 from enum import Enum
-from collections import deque
 
 TEST_FILE = f'{os.path.dirname(__file__)}/test_input'
 INPUT_FILE = f'{os.path.dirname(__file__)}/input'
@@ -34,17 +31,6 @@ def within_limits(city_map, pos):
   i, j = pos
   return i >= 0 and i < height and j >= 0 and j < width
 
-def next_cell(pos, direction: Dir):
-  i, j = pos
-  adjacents = {
-    Dir.UP: (i-1,j),
-    Dir.DOWN: (i+1,j),
-    Dir.RIGHT: (i,j+1),
-    Dir.LEFT: (i,j-1),
-  }
-  adjacents.pop(opposite(direction))
-  return adjacents.items()
-
 def viz(city_map, hashes=set()):
   height, width = len(city_map), len(city_map[0])
   for i in range(height):
@@ -65,6 +51,17 @@ def heatloss_at_pos(city_map, pos):
   i,j = pos
   return city_map[i][j]
 
+def next_nodes(pos, direction: Dir):
+  i, j = pos
+  adjacents = {
+    Dir.UP: (i-1,j),
+    Dir.DOWN: (i+1,j),
+    Dir.RIGHT: (i,j+1),
+    Dir.LEFT: (i,j-1),
+  }
+  adjacents.pop(opposite(direction))
+  return adjacents.items()
+
 MAX = 100000000000000
 def best_path(city_map, start, end):
   visited = set()
@@ -72,6 +69,7 @@ def best_path(city_map, start, end):
   to_visit = {start_node}
   heatloss = {start_node: 0}
   path_to = {start_node: []}
+
 
   def min_key(node):
     pos, _, _ = node
@@ -88,7 +86,7 @@ def best_path(city_map, start, end):
 
     visited.add(node)
 
-    for next_dir, next_pos in next_cell(pos, direction):
+    for next_dir, next_pos in next_nodes(pos, direction):
       if not within_limits(city_map, next_pos):
         continue
       if steps_in_dir > 1 and next_dir == direction:
@@ -98,9 +96,49 @@ def best_path(city_map, start, end):
 
       if next_node in visited:
         continue
+      
+      next_heatloss = heatloss[node] + heatloss_at_pos(city_map, next_pos)
+      if next_heatloss < heatloss.get(next_node, MAX):
+        heatloss[next_node] = next_heatloss
+        path_to[next_node] = path_to[node] + [pos]
 
-      heatloss[next_node] = min(heatloss.get(next_node, MAX), heatloss[node] + heatloss_at_pos(city_map, next_pos))
-      path_to[next_node] = path_to[node] + [pos]
+      to_visit.add(next_node)
+
+def best_path_2(city_map, start, end):
+  visited = set()
+  start_node = (start, Dir.RIGHT, -1)
+  to_visit = {start_node}
+  heatloss = {start_node: 0}
+  path_to = {start_node: []}
+
+  while to_visit:
+
+    node = min(to_visit, key=lambda node: heatloss[node])
+    to_visit.remove(node)
+    pos, direction, steps_in_dir = node
+
+    if pos == end:
+      return heatloss[node], path_to[node]
+
+    visited.add(node)
+
+    for next_dir, next_pos in next_nodes(pos, direction):
+      if not within_limits(city_map, next_pos):
+        continue
+      if steps_in_dir < 3 and next_dir != direction:
+        continue
+      if steps_in_dir > 8 and next_dir == direction:
+        continue
+      next_steps_in_dir = 0 if next_dir != direction else steps_in_dir + 1
+      next_node = (next_pos, next_dir, next_steps_in_dir)
+
+      if next_node in visited:
+        continue
+      
+      next_heatloss = heatloss[node] + heatloss_at_pos(city_map, next_pos)
+      if next_heatloss < heatloss.get(next_node, MAX):
+        heatloss[next_node] = next_heatloss
+        path_to[next_node] = path_to[node] + [pos]
 
       to_visit.add(next_node)
 
@@ -112,8 +150,20 @@ def part_1(file):
   print(heatloss)
   return heatloss
 
+def part_2(file):
+  city_map = parse_file(file)
+  height, width = len(city_map), len(city_map[0])
+  heatloss, path = best_path_2(city_map, (0,0), (height-1, width-1))
+  viz(city_map, set(path))
+  print(heatloss)
+  return heatloss
+
 # Solution
-print(part_1(INPUT_FILE)) # 797
+# print(part_1(INPUT_FILE)) # 797
+# print(part_2(INPUT_FILE)) # 914
 
 # Tests
-assert(part_1(TEST_FILE)) == 102
+# assert(part_1(INPUT_FILE)) == 797
+# assert(part_1(TEST_FILE)) == 102
+# assert(part_2(INPUT_FILE)) == 914
+assert(part_2(TEST_FILE)) == 94
